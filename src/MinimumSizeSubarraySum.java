49f345b43ef9568a2203557d4556b428c5bdfc75
Kadir Akın KORKUNÇ <kadirakinkorkunc@gmail.com>
2022-10-01 00:32:06 +0300

    ADD minimum size subarray sum

---
+++ src/MinimumSizeSubarraySum.java
@@ -0,0 +1,30 @@
+/**
+ * <a
+ * href="https://leetcode.com/problems/minimum-size-subarray-sum/">Minimum Size Subarray Sum</a>
+ */
+public class MinimumSizeSubarraySum {
+
+    public static void main(String[] args) {
+        int target = 1;
+        int[] nums = {1, 4, 4};
+        System.out.println(calculate(target, nums));
+    }
+
+    private static int calculate(int target, int[] nums) {
+        int maxLimit = (int)(Math.pow(10, 9) + 1);
+        int minElSize = maxLimit, leftWindow = 0, total = 0;
+        for (int rightWindow = 0; rightWindow < nums.length;) {
+            total += nums[rightWindow];
+
+            while (total >= target) {
+                minElSize = Math.min(minElSize, rightWindow - leftWindow + 1);
+                total -= nums[leftWindow];
+                leftWindow++;
+            }
+
+            rightWindow++;
+        }
+
+        return minElSize == maxLimit ? 0 : minElSize;
+    }
+}
